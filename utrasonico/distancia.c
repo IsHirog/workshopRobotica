164a6dce6a15d61d8ebe4ebab56b42586d71454e
IsHirog <chritiangabriel3d@gmail.com>
2025-11-07 15:39:40 -0300

    tava errado

--- utrasonico/distancia.c
+++ utrasonico/distancia.c
@@ -1,36 +1,33 @@
-#include <Ultrasonic.h> 
-
-#define pino_trigger 4  
-#define pino_echo 5  
-// Taxa de comunicação serial
-#define BAUD_RATE 9600
-
-Ultrasonic ultrasonic(pino_trigger, pino_echo);  
-
-void setup() {  
-  // Inicializa a comunicação serial
-  Serial.begin(BAUD_RATE); 
-  Serial.println("Monitoramento de Distancia - Sensor Ultrassonico");
-  Serial.println("----------------------------------------------");
-}  
-
-void loop() {  
-  float cmMsec, inMsec;  
-  
-  // 1. Faz a leitura do tempo do ultrassom
-  long microsec = ultrasonic.timing();  
-  
-  // 2. Converte o tempo para distancia em CM e Polegadas
-  cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);  
-  inMsec = ultrasonic.convert(microsec, Ultrasonic::IN);  
-
-  // 3. Exibe os dados no Monitor Serial
-  Serial.print("Distancia: "); 
-  Serial.print(cmMsec);        
-  Serial.print(" cm");         
-  Serial.print(" | ");
-  Serial.print(inMsec);        
-  Serial.println(" in");       // Pula uma linha no final para a próxima leitura
-
-  delay(1000); // Aguarda 1 segundo antes da próxima leitura (ajustado de 2000ms para 1000ms para um feedback mais rápido)
-}
+const int TRIG = 3;
+ const int ECHO = 2;
+ const int distancia_obstaculo = 20;
+ void setup() {
+ Serial.begin(9600);
+ pinMode(TRIG,OUTPUT);
+ pinMode(ECHO,INPUT);
+ }
+ 
+ void loop() {
+ int distancia = sensor_morcego(TRIG,ECHO);
+ if(distancia <= distancia_obstaculo){
+ Serial.print("Com obstaculo: ");
+ Serial.print(distancia);
+ Serial.println("cm");
+ }
+ else{
+ Serial.print("Sem obstaculo: ");
+ Serial.print(distancia);
+ Serial.println("cm");
+ }
+ delay(100);
+ }
+ 
+ int sensor_morcego(int pinotrig,int pinoecho){
+ digitalWrite(pinotrig,LOW);
+ delayMicroseconds(2);
+ digitalWrite(pinotrig,HIGH);
+ delayMicroseconds(10);
+ digitalWrite(pinotrig,LOW);
+ 
+ return pulseIn(pinoecho,HIGH)/58;
+ }
